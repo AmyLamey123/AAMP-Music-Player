cfee87b0fe374929e856ce25111068001df58c58
AmyLamey123 <163696228+AmyLamey123@users.noreply.github.com>
2025-02-18 18:09:04 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,13 +6,16 @@
 //You will have to let go of the slider for the change to go into effect
 
 let button;
-let slider;
+let sliderVolume, sliderPan, sliderRate;
 let dropdown;
 let combined
 let music;
 let Songs;
 let amp;
 let vol;
+let duration;
+let current;
+let playButton, stopButton, restartButton; 
 
 function setup() {
   createCanvas(400, 400);
@@ -22,18 +25,22 @@ function setup() {
   button=createButton('λ');
   button.style('font-size', '30px');
   button.style('background-color', '#fb7e14');
-  button.position(width/2-button.width, height/2-button.height);
+  button.position(width/2-button.width+5, height/2-button.height);
   button.mousePressed(PressedTheButton);
   
-  slider=createSlider(0, 1, 1, 0.01);
-  slider.position(width/2-slider.width, height/2+slider.height*8);
-  slider.style('width','300px');
-  slider.changed(()=> {
-    music.setVolume(slider.value());
+  
+  sliderVolume=createSlider(0, 1, 1, 0.01);
+  sliderVolume.position(width/2-sliderVolume.width, height/2+sliderVolume.height*10);
+  sliderVolume.style('width','300px');
+  sliderVolume.changed(()=> {
+    music.setVolume(sliderVolume.value());
   });
   
+  sliderRate = createSlider(-3,3,1,0.5);
+  sliderPan = createSlider(-1,1,0,0.1 );
+  
   dropdown=createSelect();
-  dropdown.option('Forget About Freeman');
+   dropdown.option('Forget About Freeman');
   dropdown.option("We've Got Hostiles");
   dropdown.option('Surface Tension 1');
   dropdown.option('Hazardous Environments');
@@ -48,10 +55,23 @@ function setup() {
   dropdown.changed(songChanged)
   
   Songs=[dropdown.selected(), 'mp3']
-  var combined=join(Songs,'.')
+  var combined=join(Songs,'.');
   music=loadSound(combined);
   //noStroke();
+  
+  stopButton = createButton('Stop');
+  stopButton.mousePressed(stopMusic); 
+  stopButton.style('font-size', '20px');
+  stopButton.style('background-color', '#fb7e14');
+  stopButton.position(10,50);
+  
+  restartButton = createButton('Restart')
+  restartButton.mousePressed(restartMusic);
+  restartButton.style('font-size', '20px');
+  restartButton.style('background-color', '#fb7e14');
+  restartButton.position(10,100);
 }
+
 
 function songChanged(){
   Songs=[dropdown.selected(), 'mp3']
@@ -68,20 +88,57 @@ function playSong() {
 
 function PressedTheButton(){
   if (music.isPlaying()){
-  music.stop()
+  music.pause()
   }
   else{
-  music.play()
+  music.play();
   }
 }
 
 function draw() {
   background('#292929');
-  vol=amp.getLevel()
+  noStroke();
+ 
   for(i=0;i<400;i+=25){
     for(j=0;j<400;j+=25){
       fill(vol*1000,vol*200,j/2)
       ellipse(i+random(vol*30),j+random(vol*30),vol*250)
     }
   }
+  noStroke();
+  textSize(25);
+  textAlign(CENTER);
+  text('Volume',width/2, 500);
+  strokeWeight(3);
+  
+  music.pan(sliderPan.value());
+  sliderPan.position(96,400);
+  sliderPan.style('width','300px');
+  text('Left and Right Balance',width/2, 450);
+  
+  music.rate(sliderRate.value());
+  sliderRate.position(250,250);
+  sliderRate.style('width','480px');
+  sliderRate.style('transform', 'rotate(270deg)');
+  text('S',470, 50);
+  text('p',470, 70);
+  text('e',470, 90);
+  text('e',470, 110);
+  text('d',470, 130);
+  
+  vol=amp.getLevel()
+  
+  noStroke();
+  fill('#fb7e14');
+  let rectWidth = map(current, 0, duration, 0, width);
+  rect(0, 0, rectWidth, 20);
+}
+
+function stopMusic() {
+  music.stop();
+}
+
+function restartMusic(){
+  music.stop();
+  music.play();
 }
